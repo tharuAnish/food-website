c113c39f9b612c28e7fc43620aa8f326ecdd539c
tharuAnish <namoanishtharu@gmail.com>
2024-05-28 13:02:09 +0530

    added a loading page until the recipes are fetched

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,17 @@
 import Link from "next/link"
+import { Suspense } from "react"
 
 import classes from "./page.module.css"
 import MealsGrid from "@/components/meals/meals-grid"
 import { getMeals } from "@/lib/meals"
 
-export default async function Meals() {
+export async function Meals() {
   const meals = await getMeals()
+
+  return <MealsGrid meals={meals} />
+}
+
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +27,11 @@ export default async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Loading Meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   )
